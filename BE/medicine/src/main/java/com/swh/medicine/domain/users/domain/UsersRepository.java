7be90ae03ba12fbaac4ba1d81e4220398d7f565d
DAHLIACHOI <cyj000424@naver.com>
2023-07-24 16:26:08 +0900

    [FEAT] : 디바이스 등록

---
+++ BE/medicine/src/main/java/com/swh/medicine/domain/users/domain/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.swh.medicine.domain.users.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByDevice(String device);
+}
---
+++ BE/medicine/src/main/java/com/swh/medicine/global/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.swh.medicine.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
+    DUPLICATE_CONTACT(CONFLICT, "연락처가 이미 존재합니다"),
+    DEVICE_NOT_FOUND(NOT_FOUND, "디바이스를 찾을 수 없습니다");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
