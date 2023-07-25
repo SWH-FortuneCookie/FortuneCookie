d3f9c993d6a4c77848aa019720bc25805178a325
muncaem <mooncm02@gmail.com>
2023-07-25 19:35:30 +0900

    modify detail 2 - flexboxlayout

--- FE/F_Cookie/app/src/main/java/com/example/f_cookie/MeDetailActivity.java
+++ FE/F_Cookie/app/src/main/java/com/example/f_cookie/MeDetailActivity.java
@@ -1,14 +1,20 @@
 package com.example.f_cookie;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 public class MeDetailActivity extends AppCompatActivity {
 
@@ -19,12 +25,14 @@ public class MeDetailActivity extends AppCompatActivity {
     TextView[] effect;
     TextView[] component;
     ImageView looks;
+    FlexboxLayout e_view, c_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drug_detail);
 
+        c_view = findViewById(R.id.c_view);
         backBtn = findViewById(R.id.d_backbtn);
         addMngBtn = findViewById(R.id.addlistButton);
         name = findViewById(R.id.drug_name);
@@ -54,6 +62,27 @@ public class MeDetailActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ManageActivity.class);
             startActivity(intent);
         });
+
+        TextView tv = new TextView(getApplicationContext());
+        tv.setText("테스트1");
+        tv.setTextSize(20);
+        tv.setId(0);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.topMargin = Math.round(5 * getResources().getDisplayMetrics().density);
+        tv.setLayoutParams(params);
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int size = Math.round(10 * dm.density);
+
+        tv.setPadding(size, size, size, size);
+        tv.setBackground(getResources().getDrawable(R.drawable.uses_design));
+        tv.setTypeface(null, Typeface.BOLD);
+        tv.setTextColor(Color.BLACK);
+        tv.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        c_view.addView(tv);
+
     }
 
         private void showPopupActivity() {
