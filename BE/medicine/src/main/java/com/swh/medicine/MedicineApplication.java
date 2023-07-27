0a524ac7509656438a8da1be1ac82c9257ad27d4
DAHLIACHOI <cyj000424@naver.com>
2023-07-27 16:09:57 +0900

    [ADD] : 사용자 FCM 토큰정보 DB추가

--- BE/medicine/src/main/java/com/swh/medicine/MedicineApplication.java
+++ BE/medicine/src/main/java/com/swh/medicine/MedicineApplication.java
@@ -2,10 +2,12 @@ package com.swh.medicine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling // 스케줄링을 위한 어노테이션
+@EnableAsync // 비동기 처리를 위한 어노테이션
 public class MedicineApplication {
 
 	public static void main(String[] args) {
